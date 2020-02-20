9e17467ded62571bd9de273345f8a1f92dc93dec
duddel <duddel@users.noreply.github.com>
2020-02-20 21:35:26 +0100

    readme, comments, license

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 # Lua Lab
 
-Experiments with Lua
+In this experiment, we compile `Lua` into a C++ executable and call a `Lua` function from native code.
+
+The `Lua` source code is not provided in this repository, but cloned during the build step. This is dony by CMake with `ExternalProject`, see: [CMakeLists.txt](CMakeLists.txt).
+
+Configure and build via CMake (v3.6+), like so:
+
+```cmake
+mkdir _build
+cd _build
+cmake ..
+cmake --build .
+```
+
+This builds an executable `lua-lab`. The Output of  `lua-lab`, if everythings works:
+
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
+    0.0 0.0 0.0 0.7 1.1 1.2 1.1 0.7 0.0 0.0 0.0
+    0.0 0.0 0.9 1.7 2.2 2.4 2.2 1.7 0.9 0.0 0.0
+    0.0 0.7 1.7 2.5 3.1 3.2 3.1 2.5 1.7 0.7 0.0
+    0.0 1.1 2.2 3.1 3.6 3.8 3.6 3.1 2.2 1.1 0.0
+    0.0 1.2 2.4 3.2 3.8 4.0 3.8 3.2 2.4 1.2 0.0
+    0.0 1.1 2.2 3.1 3.6 3.8 3.6 3.1 2.2 1.1 0.0
+    0.0 0.7 1.7 2.5 3.1 3.2 3.1 2.5 1.7 0.7 0.0
+    0.0 0.0 0.9 1.7 2.2 2.4 2.2 1.7 0.9 0.0 0.0
+    0.0 0.0 0.0 0.7 1.1 1.2 1.1 0.7 0.0 0.0 0.0
+    0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0 0.0
--- main.cpp
+++ main.cpp
@@ -1,3 +1,22 @@
+/*
+Copyright (c) 2020 Alexander Scholz
+
+This software is provided 'as-is', without any express or implied
+warranty. In no event will the authors be held liable for any damages
+arising from the use of this software.
+
+Permission is granted to anyone to use this software for any purpose,
+including commercial applications, and to alter it and redistribute it
+freely, subject to the following restrictions:
+
+1. The origin of this software must not be misrepresented; you must not
+   claim that you wrote the original software. If you use this software
+   in a product, an acknowledgment in the product documentation would be
+   appreciated but is not required.
+2. Altered source versions must be plainly marked as such, and must not be
+   misrepresented as being the original software.
+3. This notice may not be removed or altered from any source distribution.
+*/
 extern "C"
 {
 #include "lua.h"
@@ -7,37 +26,52 @@ extern "C"
 
 int main()
 {
-  const char luaCode[] =
-      "function f(x,y,a) \n\
-      z = a*math.cos(math.sqrt(x*x+y*y)) \n\
-      if z > 0 then \n\
-        return z \n\
-      else \n\
-        return 0 \n\
-      end \n\
-    end";
+    // the Lua code can come from anywhere (from file, command line, etc.)
+    const char luaCode[] =
+        "function f(x,y,a) \n\
+        z = a*math.cos(math.sqrt(x*x+y*y)) \n\
+        if z > 0 then \n\
+          return z \n\
+        else \n\
+          return 0 \n\
+        end \n\
+      end";
 
-  lua_State *L = luaL_newstate();
-  luaL_openlibs(L);
-  luaL_dostring(L, luaCode);
+    // create a Lua state and open standard libraries
+    lua_State *L = luaL_newstate();
+    luaL_openlibs(L);
 
-  for (float i = -1.5707963f; i <= 1.5707963f; i += 0.3141592f)
-  {
-    for (float j = -1.5707963f; j <= 1.5707963f; j += 0.3141592f)
+    // execute the Lua code from above
+    luaL_dostring(L, luaCode);
+
+    for (float i = -1.5707963f; i <= 1.5707963f; i += 0.3141592f)
     {
-      lua_getglobal(L, "f");
-      lua_pushnumber(L, i);
-      lua_pushnumber(L, j);
-      lua_pushnumber(L, 4.0f);
-      lua_call(L, 3, 1);
-      float z = lua_tonumber(L, 1);
-      printf("%.1f ", z);
-      lua_pop(L, 1);
+        for (float j = -1.5707963f; j <= 1.5707963f; j += 0.3141592f)
+        {
+            // push the Lua function f() onto the Lua stack
+            lua_getglobal(L, "f");
+
+            // push the three arguments for f() onto the Lua stack
+            lua_pushnumber(L, i);
+            lua_pushnumber(L, j);
+            lua_pushnumber(L, 4.0f);
+
+            // call f() and tell Lua that 3 arguments are provided and 1 return value is expected
+            lua_call(L, 3, 1);
+            // now, f() and the arguments are popped from the Lua stack, the return value is pushed
+
+            // get the return value of f() from the Lua stack and print it
+            float z = lua_tonumber(L, 1);
+            printf("%.1f ", z);
+
+            // pop the return value from the Lua stack
+            lua_pop(L, 1);
+        }
+        printf("\n");
     }
-    printf("\n");
-  }
 
-  lua_close(L);
+    // close the Lua state
+    lua_close(L);
 
-  return 0;
+    return 0;
 }
